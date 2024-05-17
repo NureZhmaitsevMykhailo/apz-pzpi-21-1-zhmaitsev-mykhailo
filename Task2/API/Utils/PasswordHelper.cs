a6083348da5b55c17fa79e3bf2d0f131dc7aa253
NureZhmaitsevMykhailo <mykhailo.zhmaitsev@nure.ua>
2024-05-18 01:03:12 +0200

    Create Utils PasswordHelper

---
+++ Task2/API/Utils/PasswordHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Utils;
+
+public static class PasswordHelper
+{
+    public static (string hashedPassword, string salt) HashPassword(string password)
+    {
+        var saltBytes = RandomNumberGenerator.GetBytes(16);
+        var combinedBytes = Encoding.UTF8.GetBytes(password).Concat(saltBytes).ToArray();
+        
+        using var sha256 = SHA256.Create();
+        var hashedBytes = sha256.ComputeHash(combinedBytes);
+        var hashedPassword = Convert.ToBase64String(hashedBytes);
+
+        return (hashedPassword, Convert.ToBase64String(saltBytes));
+    }
+
+    public static bool IsPasswordValid(string enteredPassword, string storedPassword, string salt)
+    {
+        var enteredPasswordBytes = Encoding.UTF8.GetBytes(enteredPassword);
+        var saltBytes = Convert.FromBase64String(salt);
+        var saltedPasswordBytes = new byte[enteredPasswordBytes.Length + saltBytes.Length];
+        Array.Copy(enteredPasswordBytes, saltedPasswordBytes, enteredPasswordBytes.Length);
+        Array.Copy(saltBytes, 0, saltedPasswordBytes, enteredPasswordBytes.Length, saltBytes.Length);
+
+        using var sha256 = SHA256.Create();
+        var hashedBytes = sha256.ComputeHash(saltedPasswordBytes);
+        var enteredHash = Convert.ToBase64String(hashedBytes);
+            
+        return string.Equals(enteredHash, storedPassword);
+    }
+}
