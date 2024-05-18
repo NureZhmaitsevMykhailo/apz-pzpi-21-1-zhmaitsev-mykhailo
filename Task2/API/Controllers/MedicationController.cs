1a2e6110554ed919ea8b5763166c4e957f0a772f
NureZhmaitsevMykhailo <mykhailo.zhmaitsev@nure.ua>
2024-05-18 02:11:46 +0200

    Added Medication and Medicine controllers

---
+++ Task2/API/Controllers/MedicationController.cs
@@ -0,0 +1,61 @@
+﻿using API.Core.Attributes;
+using API.Core.DTOs.Medication;
+using API.Services.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/medication")]
+[DoctorRoleInterceptor]
+[Authorize]
+public class MedicationController(IMedicationService medicationService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<MedicationDto>>> GetMedications()
+    {
+        var medications = await medicationService.GetMedicationsAsync();
+        return Ok(medications);
+    }
+
+    [HttpGet("{medicineId:int}")]
+    public async Task<ActionResult<MedicationDto>> GetMedicationById(int medicineId)
+    {
+        var medication = await medicationService.GetMedicationByIdAsync(medicineId);
+
+        if (medication == null)
+            return NotFound();
+
+        return Ok(medication);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> AddMedication([FromBody] MedicationCreateDto medicationDto)
+    {
+        var addedMedicationId = await medicationService.AddMedicationAsync(medicationDto);
+        return Ok(addedMedicationId);
+    }
+
+    [HttpPut("{medicineId:int}")]
+    public async Task<ActionResult<bool>> UpdateMedication(int medicineId, [FromBody] MedicationUpdateDto updatedMedicationDto)
+    {
+        var result = await medicationService.UpdateMedicationAsync(medicineId, updatedMedicationDto);
+
+        if (!result)
+            return NotFound();
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{medicineId:int}")]
+    public async Task<ActionResult<bool>> DeleteMedication(int medicineId)
+    {
+        var result = await medicationService.DeleteMedicationAsync(medicineId);
+
+        if (!result)
+            return NotFound();
+
+        return Ok(result);
+    }
+}
---
+++ Task2/API/Controllers/MedicineController.cs
@@ -0,0 +1,94 @@
+﻿using API.Core.DTOs.Medicine;
+using API.Core.DTOs.Prescription;
+using API.Services.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class MedicineController(IMedicineService medicineService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<MedicineDto>>> GetMedicines()
+    {
+        var medicines = await medicineService.GetMedicinesAsync();
+        return Ok(medicines);
+    }
+
+    [HttpGet("{medicineId:int}")]
+    public async Task<ActionResult<MedicineDto>> GetMedicineById(int medicineId)
+    {
+        var medicine = await medicineService.GetMedicineByIdAsync(medicineId);
+
+        if (medicine == null)
+            return NotFound();
+
+        return Ok(medicine);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> AddMedicine([FromBody] MedicineCreateDto medicine)
+    {
+        var addedMedicineId = await medicineService.AddMedicineAsync(medicine);
+        return Ok(addedMedicineId);
+    }
+
+    [HttpPut("{medicineId:int}")]
+    public async Task<ActionResult<bool>> UpdateMedicine(int medicineId, [FromBody] MedicineUpdateDto updatedMedicine)
+    {
+        var result = await medicineService.UpdateMedicineAsync(medicineId, updatedMedicine);
+
+        if (!result)
+            return NotFound();
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{medicineId:int}")]
+    public async Task<ActionResult<bool>> DeleteMedicine(int medicineId)
+    {
+        var result = await medicineService.DeleteMedicineAsync(medicineId);
+
+        if (!result)
+            return NotFound();
+
+        return Ok(result);
+    }
+
+    [HttpPost("takeMedicines/{userId:int}")]
+    public async Task<ActionResult<PrescriptedMedicineDtoList>> RetrieveUserMedicine(int userId)
+    {
+        List<PrescriptedMedicineDto> result;
+
+        try
+        {
+            result = await medicineService.GetPrescriptedMedicines(userId);
+        }
+        catch (ArgumentException)
+        {
+            // error in case of bad input
+            return BadRequest("Bad input");
+        }
+        catch (FieldAccessException)
+        {
+            // error in case of no medications left to take today
+            return BadRequest("No medications left to take today");
+        }
+        catch (Exception)
+        {
+            // error in case of unexpected error
+            return Problem("Unexpected error happened.");
+        }
+
+        if (result.Count < 1)
+        {
+           // not found in case if no medications to take were found
+            return NotFound("No prescribed medications were found. ");
+        }
+        
+        return Ok(new PrescriptedMedicineDtoList(result));
+    }
+}
