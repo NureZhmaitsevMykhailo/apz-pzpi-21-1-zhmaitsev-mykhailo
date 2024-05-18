897586df15fb492f0b46c49390e4c17d13df6f14
NureZhmaitsevMykhailo <mykhailo.zhmaitsev@nure.ua>
2024-05-18 02:11:28 +0200

    Added User and Doctor controllers

---
+++ Task2/API/Controllers/DoctorController.cs
@@ -0,0 +1,105 @@
+﻿using API.Core.Constants;
+using API.Core.DTOs.Doctor;
+using API.Core.Entities;
+using API.Services.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DoctorController(IDoctorService doctorService, IJwtService jwtService) : ControllerBase
+{
+    [HttpGet]
+    [Authorize]
+    public async Task<ActionResult<IEnumerable<Doctor>>> GetDoctors()
+    {
+        var doctors = await doctorService.GetDoctorsAsync();
+        return Ok(doctors);
+    }
+    
+    [HttpGet("{id:int}")]
+    [Authorize]
+    public async Task<ActionResult<Doctor>> GetDoctor(int id)
+    {
+        var doctor = await doctorService.GetDoctorAsync(id);
+        if (doctor == null)
+        {
+            return NotFound();
+        }
+        return Ok(doctor);
+    }
+    
+    [HttpPost]
+    [Authorize]
+    public async Task<ActionResult<Doctor>> CreateDoctor([FromBody] CreateDoctorDto doctor)
+    {
+        try
+        {
+            await doctorService.CreateDoctorAsync(doctor);
+            return Ok();
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+    
+    [HttpPut("{id:int}")]
+    [Authorize]
+    public async Task<IActionResult> UpdateDoctor(int id, [FromBody] UpdateDoctorDto doctor)
+    {
+        try
+        {
+            await doctorService.UpdateDoctorAsync(id, doctor);
+            return NoContent();
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+    
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login([FromBody] DoctorLoginDto doctorLoginDto)
+    {
+        var doctor = await doctorService.LogInAsync(doctorLoginDto.Email, doctorLoginDto.Password);
+
+        if (doctor == null)
+        {
+            return Unauthorized("Invalid email or password");
+        }
+        
+        var token = jwtService.GenerateToken(doctor.Id, Role.DoctorRole);
+        return Ok(token);
+    }
+    
+    [HttpPost("signup")]
+    public async Task<ActionResult> SignUp([FromBody] DoctorSignUpDto doctorSignUpDto)
+    {
+        try
+        {
+            var result = await doctorService.SignUpAsync(doctorSignUpDto);
+
+            if (result)
+            {
+                return Ok("Doctor registration successful");
+            }
+
+            return BadRequest("Doctor with the same email already exists");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal Server Error: {ex.Message}");
+        }
+    }
+}
---
+++ Task2/API/Controllers/UserController.cs
@@ -0,0 +1,121 @@
+﻿using API.Core.Constants;
+using API.Core.DTOs.User;
+using API.Core.Entities;
+using API.Services.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(IUserService userService, IJwtService jwtService) : ControllerBase
+{
+    [HttpGet]
+    [Authorize]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        var users = await userService.GetUsersAsync();
+        return Ok(users);
+    }
+    
+    [HttpGet("{id:int}")]
+    [Authorize]
+    public async Task<ActionResult<User>> GetUser(int id)
+    {
+        var user = await userService.GetUserAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return Ok(user);
+    }
+    
+    [HttpPost]
+    [Authorize]
+    public async Task<ActionResult<User>> CreateUser([FromBody] CreateUserDto user)
+    {
+        try
+        {
+            await userService.CreateUserAsync(user);
+            return Ok();
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+    
+    [HttpPut("{id:int}")]
+    [Authorize]
+    public async Task<IActionResult> UpdateUser(int id, [FromBody] UpdateUserDto user)
+    {
+
+        try
+        {
+            await userService.UpdateUserAsync(id, user);
+            return NoContent();
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+
+    [HttpDelete("{id:int}")]
+    [Authorize]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        try
+        {
+            await userService.DeleteUserAsync(id);
+            return NoContent();
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+    
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login([FromBody] UserLoginDto userLoginDto)
+    {
+        var user = await userService.LogInAsync(userLoginDto.Email, userLoginDto.Password);
+
+        if (user == null)
+        {
+            return Unauthorized("Invalid email or password");
+        }
+        
+        var token = jwtService.GenerateToken(user.Id, Role.UserRole);
+        return Ok(token);
+    }
+    
+    [HttpPost("signup")]
+    public async Task<ActionResult> SignUp([FromBody] UserSignUpDto userSignUpDto)
+    {
+        try
+        {
+            var result = await userService.SignUpAsync(userSignUpDto);
+
+            if (result)
+            {
+                return Ok("User registration successful");
+            }
+
+            return BadRequest("User with the same email already exists");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal Server Error: {ex.Message}");
+        }
+    }
+}
