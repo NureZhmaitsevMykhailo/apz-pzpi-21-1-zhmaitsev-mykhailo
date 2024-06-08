7b1c57bfcbd60b54fb29ad9d3c31efa055b0b67b
NureZhmaitsevMykhailo <mykhailo.zhmaitsev@nure.ua>
2024-06-08 12:52:14 +0200

    Created Notifications table

---
+++ Task2/API/Core/Entities/Notification.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Core.Entities;
+
+public class Notification : BaseEntity
+{
+    public string Message { get; set; }
+    public bool isRead { get; set; }
+    public int DoctorId { get; set; }
+    public Doctor Doctor { get; set; }
+}
